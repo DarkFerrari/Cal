3a0966ee89eed7d6bfda0e850c0472c40614cf82
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-05-12 22:26:45 -0500

    add delete, num and operator button functionality
    add the functionality for clicking buttons for inputing num and operators
    
    added delete button

--- calculator.js
+++ calculator.js
@@ -1,10 +1,33 @@
 const calculateButton = document.querySelector(".cal-button")
+const inputElement = document.querySelector("#user-input")
+
+//numerical buttons
+const numButtons = document.querySelectorAll(".num-button")
+numButtons.forEach(button => {
+    button.addEventListener("click",()=> inputElement.value += button.dataset.value)
+})
+
+const operatorButtons = document.querySelectorAll(".operator-button")
+operatorButtons.forEach( button => {
+    button.addEventListener("click",()=>inputElement.value += button.dataset.value )
+})
+
+//operator buttons
+const addButton = document.querySelector("#add-button")
+addButton.addEventListener("click",()=> inputElement.value += "+")
+
+const deleteButton = document.querySelector(".delete-button")
+deleteButton.addEventListener("click",()=> inputElement.value = inputElement.value.slice(0,-1))
+
+
 calculateButton.addEventListener("click",()=>{
-    const inputElement = document.querySelector("#user-input")
     const input = inputElement.value
     result = identifyOperator(input)
     inputElement.value = result
 })
+
+const clearButton = document.querySelector(".clear-button")
+clearButton.addEventListener("click",()=> inputElement.value=0)
 
 function identifyOperator(input){
     let operators = ["+","-","%","/","*"]
